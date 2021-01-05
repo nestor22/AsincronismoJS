343597d819a9a44b6abc40286079170bc90b4aff
Nestor Gonzalez <nestor.ge222@gmail.com>
2021-01-04 21:03:10 -0600

    peticiones apis y callbacks

---
+++ src/callback/index.js
@@ -0,0 +1,25 @@
+function sum(num2, num1){
+    return num1+num2;
+}
+
+function calc(num1, num2, callback){
+    return callback(num1, num2);
+}
+
+console.log(calc(1,2,sum));
+
+
+
+function date(callback){
+    console.log(new Date);
+    setTimeout(function(){
+        let date = new Date;
+        callback(date);
+    }, 3000)
+}
+
+function printDate(dateNow){
+    console.log(dateNow);
+}
+
+date(printDate);
