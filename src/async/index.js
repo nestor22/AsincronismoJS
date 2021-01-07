8e2bf9e39b491a0a3e43d5fa804334af5f8935a8
Nestor Gonzalez <nestor.ge222@gmail.com>
2021-01-06 21:00:14 -0600

    asincronismo con js y esperas con await

---
+++ src/async/index.js
@@ -0,0 +1,31 @@
+const doSomethingAsync = ()=>{
+    return new Promise((resolve, reject)=>{
+        (true)
+        ? setTimeout(()=>resolve('do something asyc'),3000)
+        :reject(new Error ('Test Error'))
+    })
+}
+
+
+const doSomethingAsync2 = async()=>{
+    const something = await doSomethingAsync();
+    console.log(something);
+}
+
+console.log('Before');
+doSomethingAsync2();
+console.log('after')
+
+const anotherFunction = async()=>{
+    try{
+        const something = await doSomethingAsync();
+        console.log(something)
+
+    }catch(error){
+        console.error(error)
+    }
+    
+}
+console.log('Before');
+anotherFunction();
+console.log('after')
